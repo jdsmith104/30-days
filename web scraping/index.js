6970ee6e85133c4332e8f53a6e91e23335e757b3
jdsmith104 <jdsmith104@outlook.com>
2022-07-22 22:19:35 +0100

    Added extra webscraping parameters

--- web scraping/index.js
+++ web scraping/index.js
@@ -14,13 +14,22 @@ async function buildExerciseInfo(link) {
   getImageFromDetailedExerciseDom(dom);
 
   // Get sets
+  getSetsFromDetailedExerciseDom(dom);
 
   // Get instructions
   getInstructionsFromDetailedExerciseDom(dom);
 
   // Get primary muscles
+  getMusclesAndEquipmentFromDetailedExerciseDom(dom);
 
   // Form
+  getFormFromDetailedExerciseDom(dom);
+
+  // Exercise benefits
+  getExerciseBenefitsFromDetailedExerciseDom(dom);
+
+  // Related exericses
+  getRelatedExercisesFromDetailedExerciseDom(dom);
 }
 
 async function getImageFromDetailedExerciseDom(dom) {
@@ -34,13 +43,65 @@ async function getImageFromDetailedExerciseDom(dom) {
 }
 
 async function getInstructionsFromDetailedExerciseDom(dom) {
-  const paragraphQuery = dom.window.document.querySelectorAll("p");
   const headerQuery = dom.window.document.querySelectorAll("h2");
   headerQuery.forEach((element) => {
     const headerText = element.textContent;
     if (headerText.includes("Instructions")) {
       const sibling = element.nextSibling.nextSibling;
       console.log(sibling?.innerHTML);
+    }
+  });
+}
+
+async function getSetsFromDetailedExerciseDom(dom) {
+  const headerQuery = dom.window.document.querySelectorAll("h2");
+  headerQuery.forEach((element) => {
+    const headerText = element.textContent;
+    if (headerText.includes("Sets And Reps")) {
+      const sibling = element.nextSibling.nextSibling;
+      console.log(sibling?.innerHTML);
+    }
+  });
+}
+
+async function getMusclesAndEquipmentFromDetailedExerciseDom(dom) {
+  const paragraphQuery = dom.window.document.querySelectorAll("p");
+  paragraphQuery.forEach((element) => {
+    const pText = element.textContent;
+    if (pText.includes("Primary muscles")) {
+      console.log(pText);
+    }
+  });
+}
+
+async function getFormFromDetailedExerciseDom(dom) {
+  const headerQuery = dom.window.document.querySelectorAll("h2");
+  headerQuery.forEach((element) => {
+    const headerText = element.textContent;
+    if (headerText.includes("Proper Form And Breathing Pattern")) {
+      const sibling = element.nextSibling.nextSibling;
+      console.log(sibling?.innerHTML);
+    }
+  });
+}
+async function getExerciseBenefitsFromDetailedExerciseDom(dom) {
+  const headerQuery = dom.window.document.querySelectorAll("h2");
+  headerQuery.forEach((element) => {
+    const headerText = element.textContent;
+    if (headerText.includes("Exercise Benefits")) {
+      const sibling = element.nextSibling.nextSibling;
+      console.log(sibling?.innerHTML);
+    }
+  });
+}
+
+async function getRelatedExercisesFromDetailedExerciseDom(dom) {
+  const headerQuery = dom.window.document.querySelectorAll("h2");
+  headerQuery.forEach((element) => {
+    const headerText = element.textContent;
+    if (headerText.includes("Related") && headerText.includes("Exercises")) {
+      const sibling = element.nextSibling.nextSibling;
+      console.log(headerText, sibling?.innerHTML);
     }
   });
 }
